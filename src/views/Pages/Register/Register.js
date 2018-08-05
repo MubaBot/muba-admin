5a9f1d43101150fc3a43cd8e22e127d548603a0d
Micalgenus <micalgenus@gmail.com>
2018-08-06 05:03:53 +0900

    Register page
    onChange
    onSubmit (template)

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Button, Card, CardBody, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { id: '', username: '', email: '', password: '', repassword: '' };
+  }
+
+  onChange = (e) => { this.setState({ [e.target.name]: e.target.value }); }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -10,7 +22,7 @@ class Register extends Component {
             <Col md="6">
               <Card className="mx-4">
                 <CardBody className="p-4">
-                  <Form>
+                  <Form onSubmit={this.onSubmit}>
                     <h1>Register</h1>
                     <p className="text-muted">Create your account</p>
                     <InputGroup className="mb-3">
@@ -19,13 +31,21 @@ class Register extends Component {
                           <i className="icon-user"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
+                      <Input autoFocus required name="id" type="text" placeholder="ID" autoComplete="id" onChange={this.onChange} />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input required name="username" type="text" placeholder="Username" autoComplete="username" onChange={this.onChange} />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>@</InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
+                      <Input required name="email" type="email" placeholder="Email" autoComplete="email" onChange={this.onChange} />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
@@ -33,7 +53,7 @@ class Register extends Component {
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input required name="password" type="password" placeholder="Password" autoComplete="new-password" onChange={this.onChange} />
                     </InputGroup>
                     <InputGroup className="mb-4">
                       <InputGroupAddon addonType="prepend">
@@ -41,21 +61,11 @@ class Register extends Component {
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                      <Input required name="repassword" type="password" placeholder="Repeat password" autoComplete="new-password" onChange={this.onChange} />
                     </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+                    <Button type="submit" color="success" block>Create Account</Button>
                   </Form>
                 </CardBody>
-                <CardFooter className="p-4">
-                  <Row>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-facebook" block><span>facebook</span></Button>
-                    </Col>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-twitter" block><span>twitter</span></Button>
-                    </Col>
-                  </Row>
-                </CardFooter>
               </Card>
             </Col>
           </Row>
