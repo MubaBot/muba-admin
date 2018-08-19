248fe6a1a7d92981e0e386b8fa888282ec61e92f
Micalgenus <micalgenus@gmail.com>
2018-08-20 03:18:39 +0900

    Logout

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -24,7 +24,7 @@ class DefaultLayout extends Component {
     return (
       <div className="app">
         <AppHeader fixed>
-          <DefaultHeader />
+          <DefaultHeader {...this.props} />
         </AppHeader>
         <div className="app-body">
           <AppSidebar fixed display="lg">
