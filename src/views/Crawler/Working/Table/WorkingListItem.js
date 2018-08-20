bcc6abc08910511c1eb6fa89d40048c460bea208
Micalgenus <micalgenus@gmail.com>
2018-08-20 22:25:39 +0900

    Working list add a tag

--- src/views/Crawler/Working/Table/WorkingListItem.js
+++ src/views/Crawler/Working/Table/WorkingListItem.js
@@ -5,7 +5,11 @@ class WorkingListItem extends Component {
   render() {
     return (
       <tr>
-        <td>{this.props.url}</td>
+        <td>
+          <a href={this.props.url} target="_blank">
+            {this.props.url}
+          </a>
+        </td>
         <td className="text-center">
           <Button block color="ghost-danger">
             삭제
